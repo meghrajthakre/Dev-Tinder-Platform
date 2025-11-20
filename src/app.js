bcdf25c5732b7c788ee3744114d67031ad0ff251
meghraj thakre <meghrajthakre444@gmail.com>
2025-11-20 07:35:45 +0530

    Done some routing in express js

--- src/app.js
+++ src/app.js
@@ -3,8 +3,32 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const cb0 = function (req, res, next) {
+  console.log('CB0')
+  next()
+}
+
+const cb1 = function (req, res, next) {
+  console.log('CB1')
+  next()
+}
+
+const cb2 = function (req, res) {
+  res.send('Hello from C!')
+}
+
+app.get('/example/c', [cb0, cb1, cb2])
+
 app.use('/home', (req,res)=>{
-    res.send('Hello World! from home route');
+    res.download('src/samfple.pdf', 'meghrajs.pdf',(err)=>{
+        if(err){
+            console.log(err);
+        res.status(400).send('Error downloading file');
+
+        }else{
+            console.log('File downloaded successfully');
+        }
+    })
 
 })
 app.use('/about', (req,res)=>{
