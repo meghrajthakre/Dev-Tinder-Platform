d29cfd89bd86d27328c5c116ff159eab8c6cae20
meghraj thakre <meghrajthakre444@gmail.com>
2025-11-23 16:53:42 +0530

    created a user uisng signup api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const connectDB = require('./config/database');
-
-
+const User = require('./models/userSchema');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.post('/signup', async(req, res) => {
+   const user = new User({
+    firstName: "Meghraj",
+    lastName: "Thakre",
+    email: "meghrajthakre@123",
+    password: "Meghraj@1234",
+    gender: "Male",
+   })
+
+    await user.save();
+
+    res.send("signUp successful");
+})
+
 
 
 connectDB()
