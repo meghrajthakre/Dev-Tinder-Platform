9774e4d14899466f7fbb29c5109598657b1fec83
meghraj thakre <meghrajthakre444@gmail.com>
2025-11-24 15:16:49 +0530

    created a rest api using express get,post, delete, patch

--- src/app.js
+++ src/app.js
@@ -4,24 +4,105 @@ const User = require('./models/userSchema');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
-})
+});
 
-app.post('/signup', async(req, res) => {
-   const user = new User({
-    firstName: "Meghraj",
-    lastName: "Thakre",
-    email: "meghrajthakre@123",
-    password: "Meghraj@1234",
-    gender: "Male",
-   })
 
+app.post('/signup', async (req, res) => {
+  const user = new User(req.body);
+  try {
     await user.save();
+    res.status(201).send("SignUp successful");
+  } catch (err) {
+    console.error("Error saving user:", err.message);
 
-    res.send("signUp successful");
+    if (err.code === 11000) {
+      return res.status(400).json({
+        success: false,
+        message: "Email already exists"
+      });
+    }
+
+  }
 })
 
+app.get('/user', async (req, res) => {
+  try {
+    const user = await User.find({ email: req.body.email });
+    if (user.length === 0) {
+      return res.status(404).send("User not found");
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    console.error("Error saving user:", err.message);
+    res.status(500).send("Internal Server Error");
+  }
+})
+
+
+app.get('/feed', async (req, res) => {
+  const users = await User.find({});
+  try {
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users:", err.message);
+    res.status(500).send("Error fetching users:", err.message);
+  }
+});
+
+
+app.delete('/user/:id', async (req, res) => {
+  const userId = req.params.id;
+  try {
+    if (userId.length !== 24) {
+      return res.status(400).send("Invalid User ID");
+    }
+    await User.findByIdAndDelete(userId);
+    res.status(200).send("User deleted successfully");
+  } catch (error) {
+    console.error("Error Deleting users:", err.message);
+    res.status(404).send("Error Deleting users:", err.message);
+  }
+})
+
+app.patch('/update/:id', async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    // Validate MongoDB ObjectId
+    if (userId.length !== 24) {
+      return res.status(400).json({ message: "Invalid User ID" });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      data: updatedUser
+    });
+
+  } catch (error) {
+    console.error("User Not Updated:", error.message);
+    res.status(500).json({
+      success: false,
+      message: "User Not Updated",
+      error: error.message
+    });
+  }
+});
 
 
 connectDB()
--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -2,12 +2,51 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    firstName: { type: String,},
-    lastName: { type: String,},
-    email: { type: String, unique: true },
-    password: { type: String,},
-    gender: { type: String,},
+    firstName: {
+        type: String,
+        required: true,
+        maxlength: 50,
+        minlength: 2
+    },
+    lastName: {
+        type: String,
+        required: true,
+        maxlength: 50,
+        minlength: 2
+    },
 
-})
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 100,
 
+    },
+    gender: {
+        type: String,
+        enum: ['male', 'female', 'other'],
+        message: 'Gender must be Male, Female, or Other'
+    },
+    age: {
+        type: Number,
+        min: 0,
+        max: 120
+    },
+    photourl: {
+        type: String,
+        default: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Ddummy&psig=AOvVaw2TYyHn6TsJwJf16E2fAQ34&ust=1764062698214000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCODV_uW7ipEDFQAAAAAdAAAAABAE'
+    },
+    skills: {
+        type: [String],
+        default: []
+    }
+
+},{timestamps: true});
 module.exports = mongoose.model('User', userSchema);
